7e1022930448a49d5828511ff5068c3bc087473d
timer12 <542233302@qq.com>
2020-06-22 20:25:58 +0800

    fix bug

--- IP2Loc/IP2Loc.py
+++ IP2Loc/IP2Loc.py
@@ -6,15 +6,15 @@ def getLoc(ip):
     reader = geoip2.database.Reader('./database/GeoLite2-City.mmdb')
     response = reader.city(ip)
     print('\n[*] Result for %s\n'%ip)
-    if response.country.name == None:
+    if response.country.name == None or ('zh-CN' not in response.country.names):
         print('[+] country:\t\t',response.country.name)
     else:
         print('[+] country:\t\t',response.country.name,response.country.names['zh-CN'])
-    if response.subdivisions.most_specific.name == None:
+    if response.subdivisions.most_specific.name == None or ('zh-CN' not in response.subdivisions.most_specific.names):
         print('[+] region:\t\t',response.subdivisions.most_specific.name)
     else:
         print('[+] region:\t\t',response.subdivisions.most_specific.name,response.subdivisions.most_specific.names['zh-CN'])
-    if response.city.name == None:
+    if response.city.name == None or ('zh-CN' not in response.city.names):
         print('[+] city:\t\t',response.city.name)
     else:
         print('[+] city:\t\t',response.city.name,response.city.names['zh-CN'])
